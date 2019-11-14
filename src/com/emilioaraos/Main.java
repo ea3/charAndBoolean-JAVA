36a1693fd497222636705bc517ca367cc5292fcf
Emilio Araos <ea3dev@gmail.com>
2019-11-13 20:12:55 -0700

    setting up repository

---
+++ src/com/emilioaraos/Main.java
@@ -0,0 +1,21 @@
+package com.emilioaraos;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        char myChar= 'D';
+        char myUniCodeChar= '\u0044';
+        System.out.println(myChar);
+        System.out.println(myUniCodeChar);
+
+        char myCopyright = '\u00A9';
+        System.out.println(myCopyright);
+
+        boolean myTrueBooleanValue = true;
+        boolean myFalseBooleanValue = false;
+        boolean isCustomerOverTwentyOne= true;
+
+
+    }
+}
